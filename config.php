767574bd2af3ac8cad649ecf0437d308df626f5f
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 16:45:41 +0530

    Test user-driven trigger & input analysis

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-if ( isset($_REQUEST['query'])) {
+if ($_REQUEST['config'] == "Send") {
 $mailto = "deb.webdev@gmail.com";
 $subject = "Testing mailer.d";
-$content = "Test Phase 3.pass";
+$content = "Test Phase 4.pass";
 $headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
 } else {
 $mailto = "deb.webdev@gmail.com";
 $subject = "Testing mailer.d";
-$content = "Test Phase 3.fail";
+$content = "Test Phase 4.fail";
 $headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
 }
 
