ce156e5c47141e9086e5a3f7c1b6ab85745ca5c7
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 16:08:31 +0530

    Testing conditional content delivery & how'll GET/POST work

--- config.php
+++ config.php
@@ -1,8 +1,15 @@
 <?php
+if ( isset($_REQUEST['query'])) {
 $mailto = "deb.webdev@gmail.com";
 $subject = "Testing mailer.d";
-$content = "Test Phase 2";
+$content = "Test Phase 3.pass";
 $headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
+} else {
+$mailto = "deb.webdev@gmail.com";
+$subject = "Testing mailer.d";
+$content = "Test Phase 3.fail";
+$headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
+}
 
 include "mailer.php";
 ?>
