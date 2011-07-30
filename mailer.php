14540b19bba403717f79dac518206125b6b077fc
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 15:47:46 +0530

    Starting to decouple Model & Controller

--- mailer.php
+++ mailer.php
@@ -1,3 +1,7 @@
 <?php
-mail( "deb.webdev@gmail.com", "Testing mailer.d", "Test Phase 0", "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n");
+$mailto = "deb.webdev@gmail.com";
+$subject = "Testing mailer.d";
+$content = "Test Phase 1";
+$headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
+mail( $mailto, $subject, $content, $headers);
 ?>
