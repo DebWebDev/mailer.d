cbcbfb71080f1ca5fad5f2ce52abdeb0531e231d
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 18:09:18 +0530

    Let's take some user-input & mail it

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
 $mailto = "deb.webdev@gmail.com";
-$subject = "Testing mailer.d";
-$content = "Test Phase 7";
-$headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
+
+if (isset($_REQUEST['name'])) { $sender_name = $_REQUEST['name']; }
+if (isset($_REQUEST['sender'])) { $sender_email = $_REQUEST['sender']; }
+if (isset($_REQUEST['subject'])) { $subject = "Testing mailer.d : " . $_REQUEST['subject']; }
+if (isset($_REQUEST['message'])) { $content = "Test Phase 7 :\r\n" . "\r\nName :" . $sender_name . "\r\nEmail :" . $sender_email . "\r\nMessage Body :\r\n" . $_REQUEST['message']; }
+$headers = "From: ". $sender_email ."\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
 
 include "mailer.php";
 ?>
