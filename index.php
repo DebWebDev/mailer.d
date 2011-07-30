492258bc42559c26fd8a35a47279145ecfa2fd9d
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 23:51:50 +0530

    Redirection implemented

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-if (isset($_REQUEST['test'])) { $data = array('name','sender','subject','message','deb.webdev@gmail.com','test@mailer.d','plain'); }
+if (isset($_REQUEST['test'])) { $data = array('name','sender','subject','message','deb.webdev@gmail.com','test@mailer.d','plain','http://google.com'); }
 
 include "parser.php";
 include "producer.php";
 include "mailer.php";
+
+header("Location: ".$data[7]);
 ?>
