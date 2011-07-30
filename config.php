8c4caf86353d44a34ce780aec8292739e7558503
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 17:46:53 +0530

    Testing deprecated from main branch

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $mailto = "deb.webdev@gmail.com";
 $subject = "Testing mailer.d";
-$content = "Test Phase 6";
+$content = "Test Phase 7";
 $headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
 
 include "mailer.php";
