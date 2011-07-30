9e3c44c385ded56b4407301cff80c9c7e110ad88
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 16:50:22 +0530

    Model-View-Controller Decoupled (in a naked way)

--- config.php
+++ config.php
@@ -1,15 +1,8 @@
 <?php
-if ($_REQUEST['config'] == "Send") {
 $mailto = "deb.webdev@gmail.com";
 $subject = "Testing mailer.d";
-$content = "Test Phase 4.pass";
+$content = "Test Phase 5";
 $headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
-} else {
-$mailto = "deb.webdev@gmail.com";
-$subject = "Testing mailer.d";
-$content = "Test Phase 4.fail";
-$headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
-}
 
 include "mailer.php";
 ?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+if (isset($_REQUEST['test'])) {
+	include "config.php";
+}
+?>
