0d13d823481ebe0881072846598a5aaa0de9bad0
Deb WebDev <deb.webdev@gmail.com>
2011-07-30 16:00:27 +0530

    Further step into the decoupling

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$mailto = "deb.webdev@gmail.com";
+$subject = "Testing mailer.d";
+$content = "Test Phase 2";
+$headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
+
+include "mailer.php";
+?>
--- mailer.php
+++ mailer.php
@@ -1,7 +1,3 @@
 <?php
-$mailto = "deb.webdev@gmail.com";
-$subject = "Testing mailer.d";
-$content = "Test Phase 1";
-$headers = "From: deb.webdev@gmail.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=utf-8\r\n";
 mail( $mailto, $subject, $content, $headers);
 ?>
